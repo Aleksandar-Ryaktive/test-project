94d8c3ac896e39097cab7e9b80a38cfbdec43778
Aleksandar <aleksandar@ryaktive.com>
2021-07-15 13:57:26 +0200

    another task created

--- my_first_app.py
+++ my_first_app.py
@@ -14,7 +14,17 @@ x = st.slider('Select a value')
 st.write(x, 'squared is', x * x)
 
 
-st.write("""
-         Cheers,
-         Aleksandar
-         """)
+
+st.title('Doublecheck')
+
+st.text('Type a number in the box below')
+
+n = st.number_input('Number', step=1)
+
+st.write(f'{n} * {n} = {n*n}')
+
+s = st.text_input('Type a name in the box below')
+
+
+
+st.write(f'Cheers, {s}')
