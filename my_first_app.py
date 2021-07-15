aef70386f50d082e28502d45461ffd146225ec08
Aleksandar <aleksandar@ryaktive.com>
2021-07-15 13:14:57 +0200

    added new files for testing

---
+++ my_first_app.py
@@ -0,0 +1,19 @@
+import streamlit as st
+
+st.write("""
+         # My first app
+         
+         Hello Ryaktivers,
+         
+         This is my very first app deployment made in Streamlit.
+         
+         Here is something you can test how it works:
+         """)
+
+x = st.slider('Select a value')
+st.write(x, 'squared is', x * x)
+
+st.write("""
+         Yours,
+         Aleksandar
+         """)
